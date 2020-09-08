646dd634d3cbc8436c8fce2e30e587869bb80c57
Cancer-yan <1825415296@qq.com>
2020-09-08 22:31:50 +0800

    add method:BaseApi DBUtil

---
+++ common/src/main/java/api/BaseApi.java
@@ -0,0 +1,54 @@
+package api;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * @version 1.0
+ * @Author Ethan
+ * @Date 2020/9/3 23:24 Create
+ */
+public class BaseApi {
+    //post请求
+    public Response post(String url, HashMap map){
+        Response res=
+                given()
+                        .formParams(map)
+                        .when().post(url)
+                        .then()
+                        .log().all()
+                        .extract().response();
+        return  res;
+    }
+    //get请求,不传参数
+    public Response get(String url){
+        Response res=
+                given()
+                        .when().get(url)
+                        .then()
+                        .log().all()
+                        .extract().response();
+        return  res;
+    }
+    //get请求,需要传参
+    public Response get(String url, HashMap map){
+        Response res=
+                given()
+                        .queryParams(map)
+                        .when().get(url)
+                        .then()
+                        .log().all()
+                        .extract().response();
+        return  res;
+    }
+    //response转json
+    public void resToJson(Response res){
+        JSONObject json = JSON.parseObject(res.getBody().asString());
+    }
+
+}
---
+++ common/src/main/java/utils/DBUtil.java
@@ -0,0 +1,49 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @version 1.0
+ * @Author Ethan
+ * @Date 2020/9/1 21:54 Create
+ */
+public class DBUtil {
+
+    EncryptUtil encrypt=new EncryptUtil();
+    BaseUtils baseUtils=new BaseUtils();
+    private String url="jdbc:mysql://xxx.xxx.xxx.xxx:3307/honeycomb?useUnicode=true&useDynamicCharsetInfo=false";
+    private String confPath=baseUtils.getPath("/src/main/resources/conf/db.conf");
+    private String username=null;
+    private String password=null;
+    private static Connection conn = null;
+
+    public void open() throws SQLException, IOException {
+        username=baseUtils.readConfig(confPath,"username");
+        password=encrypt.decryptByText(baseUtils.readConfig(confPath,"password"));
+        try {
+            //加载驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn= DriverManager.getConnection(url,username,password);
+            System.out.println("数据库连接成功");
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close(){
+        if(conn !=null){
+            try {
+                conn.close();
+                System.out.println("数据库已关闭");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
